e941ea31d17cf788927a3baaf42907aea3b8d0c0
Pragati Thakur <pragati.thakur@blenheimchalcot.com>
2023-10-05 11:10:49 +0530

    adding tuple methods

--- pythonprogramming/Tuples-Sequences-DS.py
+++ pythonprogramming/Tuples-Sequences-DS.py
@@ -4,3 +4,18 @@ v = ([1, 2, 3], [3, 2, 1])
 print(v[0][1])
 # accessing multiple elements of list via slicing
 print(v[1][0:3])
+
+# creating single ele tuple
+empty = ()
+singleton = 'hello',
+print(len(empty))
+print(singleton)
+# sequence unpacking
+tup = 12345, 54321, 'hello!'
+x, y, z = tup
+print(x, y, z)
+
+# methods
+tup1 = 1, 2, 3, 4, 5, 6, 7, 7, 7, 9, 1, 23, 3
+print(tup1.count(3))
+print(tup1.index(7))
