cb3516f8515faeb1e7b952284ffcc0d27378ae97
Pragati Thakur <pragati.thakur@blenheimchalcot.com>
2023-10-05 10:40:38 +0530

    adding datastructure files

---
+++ pythonprogramming/Tuples-Sequences-DS.py
@@ -0,0 +1,6 @@
+# Accessing elements of list in tuple
+v = ([1, 2, 3], [3, 2, 1])
+# accessing single elements of list
+print(v[0][1])
+# accessing multiple elements of list via slicing
+print(v[1][0:3])
