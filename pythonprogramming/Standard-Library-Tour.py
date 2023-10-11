7cc41269118da727d6e32e8e535e14f140add396
Pragati Thakur <pragati.thakur@blenheimchalcot.com>
2023-10-11 16:14:07 +0530

    updating regular expression code

--- pythonprogramming/Standard-Library-Tour.py
+++ pythonprogramming/Standard-Library-Tour.py
@@ -2,6 +2,8 @@
 import os
 import sys
 import argparse
+import re
+
 # gets us the current directory we are working from
 print(os.getcwd())
 # dir(os)  returns a list of all module functions
@@ -9,7 +11,6 @@ for i in dir(os):
     print(i)
 # help(os) return documentation related to the library which was written as docstring
 help(os)
-
 
 # COMMAND LINE ARGUMENTS
 # USING SYS.ARGV MODULE
@@ -42,18 +43,45 @@ parser = argparse.ArgumentParser(description=msg)
 parser.add_argument("square", help="display square of a number", type=int)
 parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
 args = parser.parse_args()
-answer = args.square**2
+answer = args.square ** 2
 if args.verbose:
     print(f"the square of {args.square} equals {answer}")
 else:
     print(answer)
 
-
 # REGULAR EXPRESSIONS
+# Find all lower case characters alphabetically between "a" and "z"
+txt = "The day we are practicing python"
+x = re.findall("[a-z]", txt)
+print(x)
 
+# Find all digit characters remember digit characters means 59 will read 5 and 9 separately
+txt1 = "That would be 59 dollar and 3 cents"
+y = re.findall("\d", txt1)
+print(y)
 
+# Using . dot to find any character except newline
+txt2 = "Hi! there\nHi! to you too"
+z = re.findall("H..", txt2)
+print(z)
 
+# using ^ to find starting word of a string and if we do not use MULTILINE then we will not get any word after newline
+a = re.findall("^there", "Lets play\nthere", re.MULTILINE)
+print(a)
 
+# using $ to find end of a string word
+b = re.findall("around$", "let go and play around")
+if b:
+    print("Yes, the string ends with 'around'")
+else:
+    print("No match")
 
+# Search for a sequence that starts with "he", followed by 0 or more  (any) characters, and an "o":
+txt3 = "Hello was it his birthday?"
+c = re.findall("He.*o", txt3)
+print(c)
 
-
+# Search for a sequence that starts with "he", followed exactly 2 (any) characters, and an "o using dot
+txt4 = "Hello, what's up\nHello how was the day\nHelllllo"
+d = re.findall("He.{2}o", txt4, re.MULTILINE)
+print(d)
