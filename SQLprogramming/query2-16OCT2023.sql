cff451083d58552604c4b27e3e999b6e1f448d5e
Pragati Thakur <pragati.thakur@blenheimchalcot.com>
2023-10-16 12:42:40 +0530

    updating orderby clause

--- SQLprogramming/query2-16OCT2023.sql
+++ SQLprogramming/query2-16OCT2023.sql
@@ -46,7 +46,46 @@ WHERE first_name LIKE "%a%";
 -- Last Name ends with 'n'
 SELECT * FROM STUDENT
 WHERE last_name LIKE "%n";
+-- Name starts with b and ends with a
+SELECT * FROM STUDENT
+WHERE first_name LIKE "B%a";
+-- Country which starts with U and has two more letters after it
+SELECT * FROM STUDENT
+WHERE Country LIKE "U__";
+-- Names that have 'a' in second place (Combining both wildcards)
+SELECT * FROM STUDENT
+WHERE first_name LIKE "_a%";
 
+/* IN Operator:
+The IN operator is a shorthand for multiple OR conditions. */
+SELECT * FROM STUDENT
+WHERE date_of_birth in ('2000-03-10', '2001-03-10');
+
+/* The BETWEEN operator is inclusive: begin and end values are included. */
+SELECT * FROM STUDENT
+WHERE date_of_birth BETWEEN '1998-01-01' AND '2003-12-31';
+
+-- Using ORDERBY to sort the result in ASC AND DESC Order
+SELECT * FROM STUDENT
+ORDER BY date_of_birth DESC;
+
+SELECT * FROM STUDENT
+ORDER BY first_name;
+
+SELECT * FROM STUDENT
+ORDER BY Country;
+
+-- First sorts by country and then with firstname where people from same country are not in order! 
+-- Checkout o/p of above and below code to understand
+SELECT * FROM STUDENT
+ORDER BY Country, first_name;
+
+-- check if any column has NULL or not
+SELECT * FROM STUDENT
+WHERE Country IS NULL;
+
+SELECT * FROM STUDENT
+WHERE GENDER IS NOT NULL;
 
 
 
