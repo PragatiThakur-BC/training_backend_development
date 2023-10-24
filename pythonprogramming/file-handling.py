456f1d886b3e3880bb076ec0dd9fcbeaa20ea957
Pragati Thakur <pragati.thakur@blenheimchalcot.com>
2023-10-24 12:58:38 +0530

    Updating file read code

--- pythonprogramming/file-handling.py
+++ pythonprogramming/file-handling.py
@@ -1,23 +1,23 @@
-# fhand = open('filehandlingexample.txt')
+fhand = open('filehandlingexample.txt')
 """
 this gives handle of file(Kind of wrapper around file)within which we can interact 
 to read, write, update any file operations.
 """
-# # print(fhand)
-# # Counting number of lines in file
-# count = 0
-# for line in fhand:
-#     count = count + 1
-# print("Num of lines in file : ", count)
+print(fhand)
+# Counting number of lines in file
+count = 0
+for line in fhand:
+    count = count + 1
+print("Num of lines in file : ", count)
 """
 We can put an if statement in
 our for loop to only print lines
 that meet some criteria
 """
-# for line in fhand:
-#     line = line.rstrip() # reason to do this when we print the print gives new line after each line to print
-#     if line.startswith('R'):
-#         print(line)
+for line in fhand:
+    line = line.rstrip() # reason to do this when we print the print gives new line after each line to print
+    if line.startswith('R'):
+        print(line)
 """
 Error handling while reading/opening file due to incorrect file name
 """
